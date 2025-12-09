fb00562dd24deb575e039048f506c09728bc1f62
b-rickert <bd.rickert52@gmail.com>
2025-12-09 12:18:41 -0500

    Added test for Produce

--- src/test/java/com/zipcodewilmington/froilansfarm/ProduceTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/ProduceTest.java
@@ -4,5 +4,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProduceTest {
-    
+
+    @Test 
+    public void testYield() {
+        Corn corn = new Corn();
+        assertEquals("Corn ears", corn.yield());
+    }
+
+    @Test
+    public void testHasBeenFertilized() {
+        Corn corn = new Corn();
+        assertFalse(corn.hasBeenFertilized());
+
+        corn.fertilize();
+        assertTrue(corn.hasBeenFertilized());
+    }
+
+    @Test
+    public void testHasBeenHarvested() {
+        Corn corn = new Corn();
+        assertFalse(corn.hasBeenHarvested());
+
+        corn.fertilize();
+        assertTrue(corn.hasBeenHarvested());
+    }
 }
